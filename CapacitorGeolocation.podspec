4dcfd1223e32f1d3fc43af4c01863bd51dee1a24
jcesarmobile <jcesarmobile@gmail.com>
2026-07-23 18:10:32 +0200

    chore!: Align plugin baselines with core (#89)

--- CapacitorGeolocation.podspec
+++ CapacitorGeolocation.podspec
@@ -11,7 +11,7 @@ Pod::Spec.new do |s|
   s.author = package['author']
   s.source = { :git => package['repository']['url'], :tag => "v#{s.version}" }
   s.source_files = 'ios/Sources/**/*.{swift,h,m,c,cc,mm,cpp}'
-  s.ios.deployment_target = '15.0'
+  s.ios.deployment_target = '16.0'
   s.dependency 'Capacitor'
   s.dependency 'IONGeolocationLib', '2.1.0'
   s.swift_version = '5.1'
--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorGeolocation",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "CapacitorGeolocation",
             targets: ["GeolocationPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "9.0.0-alpha.5"),
         .package(url: "https://github.com/ionic-team/ion-ios-geolocation.git", from: "2.1.0")
     ],
     targets: [
@@ -18,7 +18,6 @@ let package = Package(
             name: "GeolocationPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm"),
                 .product(name: "IONGeolocationLib", package: "ion-ios-geolocation")
             ],
             path: "ios/Sources/GeolocationPlugin"),
