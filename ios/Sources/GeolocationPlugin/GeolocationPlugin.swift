d265bcb17415dd48b73b4d2f400ffbbb9e8f97a2
Pedro Bilro <pedro.gustavo.bilro@outsystems.com>
2026-08-05 15:59:11 +0100

    fix(ios): Correct error when location services are off (#94)

--- ios/Sources/GeolocationPlugin/GeolocationPlugin.swift
+++ ios/Sources/GeolocationPlugin/GeolocationPlugin.swift
@@ -130,7 +130,8 @@ private extension GeolocationPlugin {
 
                 switch status {
                 case .denied:
-                    self.onLocationPermissionNotGranted(error: .permissionDenied)
+                    let error: GeolocationError = self.locationService?.areLocationServicesEnabled() == false ? .locationServicesDisabled : .permissionDenied
+                    self.onLocationPermissionNotGranted(error: error)
                 case .notDetermined:
                     self.requestLocationAuthorisation(type: .whenInUse)
                 case .restricted:
@@ -233,7 +234,12 @@ private extension GeolocationPlugin {
 
         switch locationService?.authorisationStatus {
         case .authorisedAlways, .authorisedWhenInUse: onLocationPermissionGranted()
-        case .denied: callbackManager?.sendError(.permissionDenied)
+        case .denied:
+            if locationService?.areLocationServicesEnabled() == false {
+                callbackManager?.sendError(.locationServicesDisabled)
+            } else {
+                callbackManager?.sendError(.permissionDenied)
+            }
         case .restricted: callbackManager?.sendError(.permissionRestricted)
         default: break
         }
