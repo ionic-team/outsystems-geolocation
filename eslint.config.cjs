85879d57f88aa58b8a53a33b3bddc73c020ff007
Chace Daniels <chace.daniels@outsystems.com>
2026-09-09 09:26:34 -0500

    chore: update eslint to v10 and @ionic/eslint-config to v1

---
+++ eslint.config.cjs
@@ -0,0 +1,16 @@
+const ionic = require('@ionic/eslint-config/recommended');
+
+module.exports = [
+  {
+    ignores: [
+      // eslint . --ext ts linted TypeScript only
+      '**/*.js',
+      '**/*.mjs',
+      '**/*.cjs',
+      '**/build/**',
+      '**/dist/**',
+      '**/example-app/**',
+    ],
+  },
+  ...ionic,
+];
